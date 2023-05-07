518546106e9360c224d8da54b7d87d746ae25ac0
simon <>
2023-05-07 19:12:36 +0100

    remove old sweep

--- sweep.go
+++ sweep.go
@@ -7,17 +7,10 @@ type Sweep struct{
 }
 
 func (s Sweep) Contains(a Angle) bool {
-	if s.CW.bool{
-		if s.Angle+s.CW.Angle>s.Angle{
-			return a>=s.Angle && a<s.CW.Angle
-		}
-		return a>=s.Angle || a<s.CW.Angle
-	}else{
-		if s.Angle+s.CW.Angle>s.Angle{
-			return !(a>=s.Angle && a<s.CW.Angle)
-		}
-		return !(a>=s.Angle || a<s.CW.Angle)
+	if s.Angle+s.CW.Angle>s.Angle{
+		return (a>=s.Angle && a<s.CW.Angle)==s.CW.bool
 	}
+	return (a>=s.Angle || a<s.CW.Angle)==s.CW.bool
 }
 
 func (s Sweep) interpolate(divs,i uint) float64{ 
@@ -45,48 +38,7 @@ func NewCounterClockwise(a Angle)CW{
 	return CW{a,false}
 }
 
-type sweep [2]Angle
 
-func (s sweep) interpolate(divs,i uint) float64{ 
-	return float64(s[1]-s[0])*float64(i)/float64(divs)
-}
-
-
-type SweepCW sweep
-
-func (s SweepCW) Contains(a Angle) bool {
-	if s[1]>s[0]{
-		return a>=s[0] && a<s[1]
-	}
-	return a>=s[0] || a<s[1]
-}
-
-func (s SweepCW) Intermediate(divs,i uint) Angle {
-	return s[0]+Angle(sweep(s).interpolate(divs,i))
-}
-
-type SweepCCW sweep
-
-func (s SweepCCW) Contains(a Angle) bool {
-	return !SweepCW(sweep(s)).Contains(a)
-}
-
-func (s SweepCCW) Intermediate(divs,i uint) Angle {
-	s[0],s[1]=s[1],s[0]
-	return s[1]-Angle(sweep(s).interpolate(divs,i))
-}
-
-type Range interface{
-	Contains(Angle)bool
-	Intermediate(uint,uint) Angle
-}
-
-func NewRange(start Angle, offset CW) Range{
-	if offset.bool{
-		return SweepCW{start,start+offset.Angle}
-	}
-	return SweepCCW{start,start-offset.Angle}
-}
 
 func Over(r interface{Intermediate(uint,uint) Angle}, steps uint) <-chan Angle{
 	as:=make(chan Angle)
--- sweep_test.go
+++ sweep_test.go
@@ -46,35 +46,6 @@ func ExampleSweepStepped() {
 	// 351.0000° 353.0000° 355.0000° 357.0000° 359.0000° 1.0000° 3.0000° 5.0000° 7.0000° 9.0000°
 }
 
-func ExampleSweepConains() {
-	fmt.Println(SweepCW{RightAngle,3*RightAngle}.Contains(RightAngle))
-	fmt.Println(SweepCW{3*RightAngle,2*RightAngle}.Contains(0))
-	fmt.Println(SweepCW{3*RightAngle,2*RightAngle}.Contains(RightAngle))
-	fmt.Println(SweepCW{0,2*RightAngle}.Contains(3*RightAngle))
-	fmt.Println(SweepCW{2*RightAngle,0}.Contains(RightAngle))
-	fmt.Println(SweepCW{3*RightAngle,RightAngle}.Contains(0))
-
-	fmt.Println(SweepCCW{RightAngle,3*RightAngle}.Contains(2*RightAngle))
-	fmt.Println(SweepCCW{3*RightAngle,2*RightAngle}.Contains(0))
-	fmt.Println(SweepCCW{3*RightAngle,2*RightAngle}.Contains(RightAngle))
-	fmt.Println(SweepCCW{0,2*RightAngle}.Contains(3*RightAngle))
-	fmt.Println(SweepCCW{2*RightAngle,0}.Contains(RightAngle))
-	fmt.Println(SweepCCW{3*RightAngle,RightAngle}.Contains(0))
-	// Output:
-	// true
-	// true
-	// true
-	// false
-	// false
-	// true	
-	// false
-	// false
-	// false
-	// true	
-	// true	
-	// false
-}
-
 func ExampleSweepDivided() {
 	s:=Sweep{RightAngle,NewClockwise(RightAngle)}
 	fmt.Printf("%+.4t %+.4t %+.4t\n",s.Intermediate(90,0),s.Intermediate(90,45),s.Intermediate(90,90))
@@ -89,22 +60,6 @@ func ExampleSweepDivided() {
 	// 0.7500⟳ 0.1250⟳ 0.5000⟳
 	// 10.0000° 180.0000° 350.0000°
 	// 350.0000° 360.0000° 10.0000°
-}
-
-func ExampleSweepDivided2() {
-	s:=SweepCCW{3*RightAngle,2*RightAngle}
-	fmt.Printf("%+.4t %+.4t %+.4t\n",s.Intermediate(90,0),s.Intermediate(90,45),s.Intermediate(90,90))
-	s=SweepCCW{2*RightAngle,3*RightAngle}
-	fmt.Printf("%+.4t %+.4t %+.4t\n",s.Intermediate(90,0),s.Intermediate(90,45),s.Intermediate(90,90))
-	s=SweepCCW{10*Degree,350*Degree}
-	fmt.Printf("%+.4d %+.4d %+.4d\n",s.Intermediate(90,0),s.Intermediate(90,45),s.Intermediate(90,90))
-	s=SweepCCW{350*Degree,10*Degree}
-	fmt.Printf("%+.4d %+.4d %+.4d\n",s.Intermediate(90,0),s.Intermediate(90,45),s.Intermediate(90,90))
-	// Output:
-	// 0.7500⟳ 0.6250⟳ 0.5000⟳
-	// 0.5000⟳ 0.1250⟳ 0.7500⟳
-	// 10.0000° 360.0000° 350.0000°
-	// 350.0000° 180.0000° 10.0000°
 }
 
 func ExampleSweepCCWDivided() {
