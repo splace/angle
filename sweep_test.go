814bd54fccc8accc9fe535eacd59ab0c908bedb7
simon <>
2023-05-08 15:47:57 +0100

    expose/hide package local angles

--- sweep_test.go
+++ sweep_test.go
@@ -31,17 +31,6 @@ func ExampleSweepContains() {
 	// false
 }
 
-// range clockwise 20 gradians from 390 gradians, show degree.
-// Note 10gradiens == 9degrees, so 9 divisions splitting 20 gradiens should be 10 angles in 2 degree steps.
-func ExampleSweepStepped() {
-	s := Gradian * 10 // slightly more precise than using upward multiplier ie Gradian*390
-	for a := range Over(Sector{-s, To{Gradian * 20, CW}}, 9) {
-		fmt.Printf("%+.4v ", a)
-	}
-	// Output:
-	// 351.0000° 353.0000° 355.0000° 357.0000° 359.0000° 1.0000° 3.0000° 5.0000° 7.0000° 9.0000°
-}
-
 func ExampleSweepDivided() {
 	s := Sector{RightAngle, To{RightAngle, CW}}
 	fmt.Printf("%+.4t %+.4t %+.4t\n", s.Intermediate(90, 0), s.Intermediate(90, 45), s.Intermediate(90, 90))
