b7b09cb586bf0517971089c2a46847175c15c38b
simon <>
2023-05-18 14:23:56 +0100

    fix reverse sweep

--- angle_test_test.go
+++ angle_test_test.go
@@ -8,9 +8,15 @@ import . "../angle"  // remove 'go.mod' for local testing.
 // Notice: 10gradians == 9degrees, so 9 divisions splitting 20 gradiens should be 10 angles in 2 degree steps.
 func ExampleAngle_testRangeOverSector() {
 	for a := range Over(Sector{Gradian*390, Gradian * 20, CW}, 9) {
-		fmt.Printf("%+.4v ", a)
+		fmt.Printf("%+.3v ", a)
 	}
+	fmt.Println()
+	for a := range Over(Sector{Gradian*10, Gradian * 380, CCW}, 9) {
+		fmt.Printf("%+.3v ", a)
+	}
+	fmt.Println()
 	// Output:
-	// 351.0000° 353.0000° 355.0000° 357.0000° 359.0000° 1.0000° 3.0000° 5.0000° 7.0000° 9.0000°
+	// 351.000° 353.000° 355.000° 357.000° 359.000° 1.000° 3.000° 5.000° 7.000° 9.000° 
+	// 9.000° 7.000° 5.000° 3.000° 1.000° 359.000° 357.000° 355.000° 353.000° 351.000° 
 }
 
