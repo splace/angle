2aa780da07c58242579db26c24f4d75b1210f05b
simon <>
2023-05-19 00:55:10 +0100

    comment

--- sweep.go
+++ sweep.go
@@ -1,15 +1,19 @@
 package angle
 
-// distinguishing type for Angle's that have, potentially, problem-space defined zero, so can/should be directly accessible.
-// notice: multiplying Angle's (by a number never another Angle) is fine, but multiplying angle's is not. like a type violation. this is because angle's have a common 'reference' zero not a defined scaling center. they are like time, in that it should be able to change the 'solution space' value that represents zero.
+// Angle exposes an angle type for problem-space angles.
+// this is akin to time/duration (Angles commonly will be angle differences.)
+// multiplying a duration is fine, but multiplying time is not the best type safety. hence here, unlike time/duration, angle is package-local.
+// angle's (like time) have a common 'reference' zero but not a defined scaling center. making it possible to change the 'solution space' value that represents zero.
+// Angle's (like duration) have a problem-space and a scaling center zero that are the same as the solution-space zero and so can be multipled.
+// Example Sector: doubling the From (angle) makes no sense in the problem-space, but doubling the Width (Angle) clearly represents twice the sector size. 
 type Angle angle
 
-// an angular region from an angle to an offset Angle in either direction.
-// notice: Angle (offset) uses From as its zero. so is relative but always positive. small CCW sweeps require a large (1 rotation - offset == -offset) Angle.
-// this allows sweeps of upto 1 rotation in either direction, using a vars sign to indicate direction would only allow upto half a rotation in either direction.
+// Sector is an angular region From an angle of a Width Angle, in either direction.
+// notice: Width Angle is always positive. small CCW sweeps are stored as a large Angle. (because of the modular behaviour this large Angle is simply -Angle))
+// this allows sweeps of upto 1 rotation in either direction, using a signed var to indicate direction would only allow upto half a rotation in either direction.
 type Sector struct {
 	From angle
-	Angle
+	Width Angle
 	Direction
 }
 
@@ -32,11 +36,11 @@ const (
 
 
 func (s Sector) Contains(a angle) bool {
-	if s.From+angle(s.Angle) > s.From {
-		return (a >= s.From && a < angle(s.Angle)) == s.Direction
+	if s.From+angle(s.Width) > s.From {
+		return (a >= s.From && a < angle(s.Width)) == s.Direction
 	}
 	// sector crosses zero.
-	return (a >= s.From || a < angle(s.Angle)) == s.Direction
+	return (a >= s.From || a < angle(s.Width)) == s.Direction
 }
 
 func interpolate(a angle, divs, i uint) angle {
@@ -46,9 +50,9 @@ func interpolate(a angle, divs, i uint) angle {
 // return the angle for the indexed division
 func (s Sector) Intermediate(divs, i uint) angle {
 	if s.Direction {
-		return s.From + interpolate(angle(s.Angle), divs, i)
+		return s.From + interpolate(angle(s.Width), divs, i)
 	}
-	return s.From - interpolate(-angle(s.Angle), divs, i)
+	return s.From - interpolate(-angle(s.Width), divs, i)
 }
 
 // return a sequence of Angle's (one more than steps) evenly dividing a sector
