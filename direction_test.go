d4fb0725d8110fffd75efa3d23cfb356a7887b13
simon <>
2023-06-01 16:19:52 +0100

    comment

--- direction_test.go
+++ direction_test.go
@@ -3,16 +3,16 @@ package angle
 import "fmt"
 
 func ExampleDirection() {
-	f:="%c (%+.1[1]d)\n"
+	f:="%c (%+.2[1]d)\n"
 	fmt.Printf(f, Direction(0))
 	fmt.Printf(f, Direction(RightAngle))
 	fmt.Printf(f, Direction(RightAngle/2))
 	fmt.Printf(f, Direction(Degree*348))
 	fmt.Printf(f, Direction(Degree*349))
 	// Output:
-	// N (0.0°)
-	// E (90.0°)
-	// NE (45.0°)
-	// NNW (348.0°)
-	// N (349.0°)
+	// N (0.00°)
+	// E (90.00°)
+	// NE (45.00°)
+	// NNW (348.00°)
+	// N (349.00°)
 }
