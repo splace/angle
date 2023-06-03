92cd2c9a066f7cf5cbc09d1ae70506139ea63eca
simon <>
2023-06-03 22:07:42 +0100

    comment

--- phase_test.go
+++ phase_test.go
@@ -2,7 +2,7 @@ package angle
 
 import "fmt"
 
-func ExampleAnglePrint() {
+func ExamplePhasePrint() {
 	fmt.Printf("%.1v degrees == %+[1]r == %+[1]v == %+.0[1]l == %+.2[1]f\n", Radian)
 	fmt.Printf("%.1v degrees == %+[1]㎭ == %+.0[1]l == %+.1[1]g == %+.2[1]f\n", RightAngle)
 	// Output:
@@ -10,7 +10,7 @@ func ExampleAnglePrint() {
 	// 90.0 degrees == 1.5707964㎭ == 90°0′0″ == 100.0ᵍ == 25.00%
 }
 
-func ExampleAngle() {
+func ExamplePhase() {
 	var ra = RightAngle
 	fmt.Printf("%+.2d\n", ra)
 	fmt.Printf("%+.2d\n", 0-ra)
@@ -21,25 +21,25 @@ func ExampleAngle() {
 	// 90.00°
 }
 
-func ExampleAngleMin() {
+func ExamplePhaseMin() {
 	fmt.Printf("%+.5s == %.4rμ㎭\n", Phase(1), Phase(1)*1000000)
 	// Output:
 	// 0.00030″ == 0.0015μ㎭
 }
 
-func ExampleAngleBinary() {
+func ExamplePhaseBinary() {
 	fmt.Printf("%+.2[1]b == %+.0[1]l\n", Phase(1<<(bits-8)))
 	// Output:
 	// 1.00b == 1°24′23″
 }
 
-func ExampleAngleMultiply() {
+func ExamplePhaseMultiply() {
 	fmt.Printf("%+.4d\n", Second*60*60)
 	// Output:
 	// 1.0000°
 }
 
-func ExampleAngleMultiplyOverflow() {
+func ExamplePhaseMultiplyOverflow() {
 	a := Degree // have to use var in order to prevent out of range message
 	fmt.Printf("%+.3d\n", a*1000)
 	fmt.Printf("%+.3d\n", -a*1000)
@@ -48,13 +48,13 @@ func ExampleAngleMultiplyOverflow() {
 	// 80.000°
 }
 
-func ExampleAngleTurns() {
+func ExamplePhaseTurns() {
 	fmt.Printf("%+.2t\n", RightAngle)
 	// Output:
 	// 0.25⟳
 }
 
-func ExampleAngleRotations() {
+func ExamplePhaseRotations() {
 	fmt.Printf("%+.2t\n", Rotations(.5))
 	fmt.Printf("%+.2t\n", Rotations(-2.5))
 	// Output:
